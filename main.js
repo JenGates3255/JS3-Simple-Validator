a4c6e261bb1e5fc768c9353a9ab852ebae32f25d
JenGates3255 <Jengates3255@gmail.com>
2014-06-24 11:02:58 -0600

    complete

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-// var number = prompt("Enter in your 10 digit phone number with dashes");
-// 	alert(number.charAt(3, 7) === '-' && number.length === 12);
+var number = prompt("Enter in your 10 digit phone number with dashes");
+	alert(number.charAt(3, 7) === '-' && number.length === 12);
 
-// var date = prompt("Enter in your 6 digit birthday with slashes");
-// 	alert(date.charAt(2,5) ==='/' && date.length ===8);
+var date = prompt("Enter in your 6 digit birthday with slashes");
+	alert(date.charAt(2,5) ==='/' && date.length ===8);
 
-// var postcode= prompt("Enter in 5 or 9 digit postalcode");
-// 	alert(postcode.charAt(5) === '-' && postcode.length === 10 || postcode.length === 5);
+var postcode= prompt("Enter in 5 or 9 digit postalcode");
+	alert(postcode.charAt(5) === '-' && postcode.length === 10 || postcode.length === 5);
 
 var state = prompt("Enter in state");
     alert(state.toUpperCase().substring(0,2));
-	
+
+var married = prompt("married");
+	alert(married.toLowerCase() === 'yes'|| married.toLowerCase() === 'no');	
