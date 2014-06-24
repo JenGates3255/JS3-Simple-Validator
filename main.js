d99a9a35dc4bf4c213fd91c2eef2b6df6536e912
JenGates3255 <Jengates3255@gmail.com>
2014-06-24 08:57:52 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// var number = prompt("Enter in your 10 digit phone number with dashes");
+// 	alert(number.charAt(3, 7) === '-' && number.length === 12);
+
+// var date = prompt("Enter in your 6 digit birthday with slashes");
+// 	alert(date.charAt(2,5) ==='/' && date.length ===8);
+
+// var postcode= prompt("Enter in 5 or 9 digit postalcode");
+// 	alert(postcode.charAt(5) === '-' && postcode.length === 10 || postcode.length === 5);
+
+var state = prompt("Enter in state");
+    alert(state.toUpperCase().substring(0,2));
+	
